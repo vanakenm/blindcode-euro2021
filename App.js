be217be183ed447475cb259c6318858e58b85ba2
Martin Van Aken <martin@joyouscoding.com>
2021-06-20 10:22:41 +0200

    v1

--- App.js
+++ App.js
@@ -2,10 +2,33 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+function Match(props) {
+  let result
+  if(props.score1 === props.score2) {
+    result = "Egalité"
+  } else if(props.score1 > props.score2) {
+    result = "Victoire " + props.team1
+  } else {
+    result = "Victoire " + props.team2
+  }
+  return (
+    <>
+      <Text>{props.team1} {props.score1} - {props.score2} {props.team2}</Text>
+      <Text>{result}</Text>
+    </>
+  )
+}
+
 export default function App() {
+  let matches = [
+    {team1: "Belgique", team2: "France", score1: 4, score2: 2},
+    {team1: "Espagne", team2: "Belgique", score1: 1, score2: 2},
+    {team1: "Allemagne", team2: "Estonie", score1: 2, score2: 2}
+  ]
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {matches.map((m) => <Match {...m} />)}
       <StatusBar style="auto" />
     </View>
   );
